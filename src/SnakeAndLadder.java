911a9ef50a3ce7c27c2306bb8b3491d816d8613a
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-06-27 00:18:54 +0530

    [Saravanan]: Added Player Start Point in SnakeAndLadderProgram

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,6 +1,12 @@
 public class SnakeAndLadder {
+
+    //Initialising a variable to hold the value of position of player on the board
+    public static final int position = 0;
     public static void main(String[] args) {
         System.out.println("Welcome To Snake And Ladder Game");
-        
+
+        System.out.println("The Starting Position of the Player is "+position);
+
+
     }
 }

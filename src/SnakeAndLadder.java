e518ab9329dc0a6450a636522b1c60ad8ba27652
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-06-28 12:45:22 +0530

    [Saravanan]: Added Player position to not go below zero program

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -10,45 +10,42 @@ public class SnakeAndLadder {
         // Using random method to generate a random number between 1 and 6
         Random r = new Random();
 
-        // Using random method to generate a random number between 1 and 3
-        int option = r.nextInt(1,4);
-        switch (option){
-            case 1:
-                optionValue = "No Play";
-                break;
-            case 2:
-                optionValue = "Ladder";
-                break;
-            case 3:
-                optionValue = "Snake";
-                break;
-            default:
-                break;
+        while (position < 100) {
+            // Using random method to generate a random number between 1 and 3
+            int option = r.nextInt(1, 4);
+            switch (option) {
+                case 1:
+                    optionValue = "No Play";
+                    break;
+                case 2:
+                    optionValue = "Ladder";
+                    break;
+                case 3:
+                    optionValue = "Snake";
+                    break;
+                default:
+                    break;
+            }
+
+
+            // Using random method to generate a random number between 1 and 6
+            int roll = r.nextInt(6) + 1;
+            System.out.println("The value of the die roll is " + roll);
+
+            if (optionValue == "No Play") {
+                System.out.println("No Play Option rolled. Skip this turn");
+            } else if (optionValue == "Ladder") {
+                System.out.println("Ladder Option rolled. Move Forward");
+                position = position + roll;
+            } else {
+                System.out.println("Snake Option rolled. Move Backwards");
+                position = position - roll;
+                if (position < 0){
+                    position = 0;
+                }
+            }
+            System.out.println("The Position of the Player is " + position);
+
         }
-
-
-
-        // Using random method to generate a random number between 1 and 6
-        int roll = r.nextInt(6)+1;
-        System.out.println("The Starting Position of the Player is "+position);
-        System.out.println("The value of the die roll is "+roll);
-
-        if(optionValue == "No Play")
-        {
-            System.out.println("No Play Option rolled. Skip this turn");
-        }else if (optionValue == "Ladder")
-        {
-            System.out.println("Ladder Option rolled. Move Forward");
-            position = position + roll;
-            System.out.println("New position is " +position);
-        }
-        else
-        {
-            System.out.println("Snake Option rolled. Move Backwards");
-            position = position - roll;
-            System.out.println("New position is " +position);
-        }
-
-
     }
 }
